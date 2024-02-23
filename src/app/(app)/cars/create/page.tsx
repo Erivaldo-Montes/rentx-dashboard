f8e82cd02d4bf9fb72c730cb1c207c2546409c86
erivaldo <erivaldo@pop-os.localdomain>
2024-02-22 21:44:57 -0300

    form

--- src/app/(app)/cars/create/page.tsx
+++ src/app/(app)/cars/create/page.tsx
@@ -11,11 +11,19 @@ import { Input } from '@/components/input'
 import { useDropzone } from 'react-dropzone'
 import Image from 'next/image'
 import { v4 as uuidV4 } from 'uuid'
+import { Loading } from '@/components/loading'
 
 enum CategoriesEnum {
   SUV = 'SUV',
   sedan = 'sedan',
   espotivo = 'esportivo',
+}
+
+type Category = {
+  id: string
+  name: string
+  description: string
+  create_at: string
 }
 
 const schema = z.object({
@@ -24,17 +32,20 @@ const schema = z.object({
   daily_rate: z.string().min(1, 'Diária é obrigatória'),
   license_plate: z.string().min(1, 'Placa é obrigatório'),
   about: z.string(),
-  category: z.nativeEnum(CategoriesEnum),
 })
 
 type CreateCarDataSchema = z.infer<typeof schema>
 
 export default function CreateCar() {
-  const router = useRouter()
   const [daily, setDaily] = useState<string>('')
   const [files, setFiles] = useState<any[]>([])
   const containerRef = useRef<HTMLDivElement>(null)
+  const [isPending, setIsPending] = useState(false)
+  const [categories, setCategories] = useState<Category[]>([])
+  const [selectedCategory, setSelectedCategory] = useState<Category>()
+  const [categoryError, setCategoryError] = useState(false)
 
+  const router = useRouter()
   const { getRootProps } = useDropzone({
     accept: {
       'image/*': [],
@@ -72,19 +83,54 @@ export default function CreateCar() {
     },
   })
 
-  function handleCreateCar(data: CreateCarDataSchema) {
+  async function handleCreateCar(data: CreateCarDataSchema) {
+    setIsPending(true)
     const value = data.daily_rate.match(/\d+/g)
+
+    console.log(selectedCategory)
+
+    // validate category
+    if (!selectedCategory) {
+      setCategoryError(true)
+      setIsPending(false)
+      return
+    } else {
+      setCategoryError(false)
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 3000))
 
     if (value) {
       const valueString = value.join('')
-      const valueFormatedInCurrency = parseInt(valueString, 10)
+      data.daily_rate = valueString
 
-      console.log(data, valueFormatedInCurrency / 100)
+      await fetch('/api/car/create', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: data.name,
+          brand: data.brand,
+          daily_rate: Number(valueString),
+          license_plate: data.license_plate,
+          category_id: selectedCategory?.id,
+        }),
+      })
     }
+
+    setIsPending(false)
   }
 
   function handleBack() {
     router.back()
+  }
+
+  function handleSelectedChange(event: ChangeEvent) {
+    const value = event.target.value
+    const categoryChange = categories.find((item) => item.name === value)
+    console.log(categoryChange)
+    setSelectedCategory(categoryChange)
   }
 
   function formatToCurrency(input: string) {
@@ -109,7 +155,18 @@ export default function CreateCar() {
   function handleRemoveImages(id: string) {
     const NewArrayFiles = files.filter((item) => item.id !== id)
     setFiles(NewArrayFiles)
-    console.log(NewArrayFiles)
+  }
+
+  async function getCategories() {
+    try {
+      const response = await fetch(`/api/car/categories`, { method: 'GET' })
+
+      const data = await response.json()
+
+      setCategories(data.categories)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   useEffect(() => {
@@ -122,7 +179,6 @@ export default function CreateCar() {
       if (container) {
         container.scrollLeft += event.deltaY
         event.preventDefault()
-        console.log(container.scrollLeft)
       }
     }
 
@@ -139,7 +195,10 @@ export default function CreateCar() {
     setValue('daily_rate', daily)
   }, [daily, setValue])
 
-  console.log(files)
+  useEffect(() => {
+    getCategories()
+  }, [])
+
   return (
     <div className="flex flex-col h-full bg-gray-100">
       <div
@@ -207,17 +266,21 @@ export default function CreateCar() {
           <div className="flex flex-col gap-2 col-span-1 ">
             <p className="text-sm">Categoria</p>
             <select
+              onChange={handleSelectedChange}
               defaultValue={''}
-              className={`bg-white w-full p-2 rounded-lg outline-gray-300 ${errors.category && 'border-red-600 border-2 outline-red-600'}`}
-              {...register('category')}
+              className={`bg-white w-full p-2 rounded-lg outline-gray-300 ${categoryError && 'border-red-600 border-2 outline-red-600'}`}
             >
               <option value={'selecione'}>Selecione</option>
-              <option value="SUV">SUV</option>
-              <option value="sedan">sedan</option>
-              <option value="esportivo">esportivo</option>
+              {categories.map((item) => {
+                return (
+                  <option value={item.name} key={item.id}>
+                    {item.name}
+                  </option>
+                )
+              })}
             </select>
 
-            {errors.category && (
+            {categoryError && (
               <span className="text-red-600 text-sm">
                 Selecione uma categoria
               </span>
@@ -336,10 +399,15 @@ export default function CreateCar() {
 
         <div className="w-full flex justify-center mt-10">
           <button
-            className="rounded-lg bg-green-600 px-10 py-2 text-white mb-10"
+            type="submit"
+            className=" flex rounded-lg bg-green-600 w-[7rem] py-2 justify-center text-white mb-10"
             onClick={handleSubmit(handleCreateCar)}
           >
-            Criar
+            {isPending ? (
+              <Loading />
+            ) : (
+              <p className="text-md text-center">Criar</p>
+            )}
           </button>
         </div>
       </div>
