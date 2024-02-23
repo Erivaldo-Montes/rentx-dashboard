647e7208d315355408b8e9452c8e1c28a77d4d3f
erivaldo <erivaldo@pop-os.localdomain>
2024-02-22 21:48:19 -0300

    fix background color

--- src/app/(app)/(dashboard)/cars/page.tsx
+++ src/app/(app)/(dashboard)/cars/page.tsx
@@ -7,8 +7,8 @@ export default function Cars() {
   return (
     <div>
       <Header />
-      <main className="sm:pl-[10rem] sm:pr-[5rem] max-sm:pr-[3rem] max-sm:pl-[6rem] lg:px-[10rem] xl:px-[20rem] mt-10">
-        <div className="flex justify-between ">
+      <main className="sm:pl-[10rem] sm:pr-[5rem] max-sm:pr-[3rem] max-sm:pl-[6rem] lg:px-[10rem] xl:px-[20rem] mt-10 bg-gray-100 pb-10">
+        <div className="flex justify-between">
           <Link href={'/cars/create'}>
             <button className="p-3 flex flex-row bg-blue-600 rounded-lg sm:w-[5rem]  gap-1 hover:bg-blue-500 transition-colors">
               <Plus color="#FFFFFF" width={20} height={20} />
