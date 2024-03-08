b35c39c6ec31200845ca4de25beaa7fde04cb791
erivaldo <erivaldo@pop-os.localdomain>
2024-03-07 21:48:39 -0300

    add credentials

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,8 @@ import NextAuth from 'next-auth'
 import { authConfig } from '../auth.config'
 import Credencial from 'next-auth/providers/credentials'
 import { api } from './axios'
-
+import { cookies } from 'next/headers'
+import { AUTH_REFRESH_COOKIE, AUTH_TOKEN_COOKIE } from '@/cookies/cookie-config'
 import { z } from 'zod'
 
 export const {
@@ -28,21 +29,25 @@ export const {
         if (parserdCredentials.success) {
           const { email, password } = credentials
 
-          const response = await api.post('/session', { email, password })
+          try {
+            const response = await api.post('/session', { email, password })
+            if (response.status === 200) {
+              // eslint-disable-next-line camelcase
+              const { token, refresh_token } = response.data
 
-          if (response.status === 401) {
-            return null
-          }
+              cookies().set(AUTH_TOKEN_COOKIE, token)
+              cookies().set(AUTH_REFRESH_COOKIE, refresh_token)
 
-          if (response.status === 200) {
-            const user = await api('/me', {
-              headers: { Authorization: `Bearer ${response.data.token}` },
-            })
+              const user = await api('/me', {
+                headers: { Authorization: `Bearer ${response.data.token}` },
+              })
 
-            if (user.data) {
-              console.log('user.data', user.data)
-              return user.data
+              if (user.data) {
+                return user.data
+              }
+              return null
             }
+          } catch (error) {
             return null
           }
         }
